99ccdfe78f2e5892b473c3cfa4a4becd8d224166
shifengren <1944832385@qq.com>
2017-12-27 15:39:22 +0800

    Update Main class

--- src/memu/Main.java
+++ src/memu/Main.java
@@ -9,52 +9,52 @@ public class Main {
 
 	public static void main(String[] args) throws Exception{
 		
-//		String location = args[0]; // specify the folder containing quantityDB and its profit table.
-//		String fileName=args[1]; // quantityDB name
-//		String quantityDBPath = location+fileName+".txt"; // unique quantityDB address
-//		String profitPath=location+fileName+"_UtilityTable.txt";// unique profit table address
-//		int beta=Integer.parseInt(args[2]); // used to generate MAU for each of item
-//		int GLMAU = Integer.parseInt(args[3]); // global least minimum average utility
-//        String HAUIsPath = args[4]; // specify the file storing the discovered HAUIs. If not specify, enter `null`
-//        String statusPath = args[5]; // specify the file storing the running status of EHAUPM. If not specify, enter `null`
-//
-//
-//		long startTimestamp = System.currentTimeMillis();
-//
-//		MEMU memu = new MEMU();
-//		memu.runAlgorithm(HAUIsPath, profitPath, quantityDBPath, beta, GLMAU);
-//
-//
-//		long endTimestamp = System.currentTimeMillis();
-//
-//        BufferedWriter statusWriter = null;
-//        if(statusPath==null || statusPath.equalsIgnoreCase("null")){
-//            statusWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-//        }else {
-//            statusWriter = new BufferedWriter(new FileWriter(statusPath));
-//        }
-//
-//        statusWriter.write(String.format("Total_time(s): %.2f\n", (endTimestamp - startTimestamp) / (double)1000));
-//        statusWriter.write(String.format("Memory_usage:%.2f\n", memu.mur.maxConsumationMemory));
-//        statusWriter.write("Haui_count:"+ memu.hauiCount +"\n");
-//        statusWriter.write("Join_count:"+ memu.candidateCount +"\n");
+		String location = args[0]; // specify the folder containing quantityDB and its profit table.
+		String fileName=args[1]; // quantityDB name
+		String quantityDBPath = location+fileName+".txt"; // unique quantityDB address
+		String profitPath=location+fileName+"_UtilityTable.txt";// unique profit table address
+		int beta=Integer.parseInt(args[2]); // used to generate MAU for each of item
+		int GLMAU = Integer.parseInt(args[3]); // global least minimum average utility
+        String HAUIsPath = args[4]; // specify the file storing the discovered HAUIs. If not specify, enter `null`
+        String statusPath = args[5]; // specify the file storing the running status of EHAUPM. If not specify, enter `null`
 
-		exe();
-	}
-	
-	
-	public static void exe() throws Exception{
-		String location = "E:\\Data_mining\\dataset\\have_value_table\\";
-		String fileName= "retail";
-		String quantityDBPath = location+fileName+".txt";
-		String profitPath=location+fileName+"_UtilityTable.txt";
-		String HAUIsPath = "E:\\java_workspace\\MEMU\\output\\with_lub.txt";
-		int GLMAU = 50000; // global least minimum average utility
-		int beta= 800;
 
 		long startTimestamp = System.currentTimeMillis();
 
-		// Applying the HUIMiner algorithm
+		MEMU memu = new MEMU();
+		memu.runAlgorithm(HAUIsPath, profitPath, quantityDBPath, beta, GLMAU);
+
+
+		long endTimestamp = System.currentTimeMillis();
+
+        BufferedWriter statusWriter = null;
+        if(statusPath==null || statusPath.equalsIgnoreCase("null")){
+            statusWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        }else {
+            statusWriter = new BufferedWriter(new FileWriter(statusPath));
+        }
+
+        statusWriter.write(String.format("Total_time(s): %.2f\n", (endTimestamp - startTimestamp) / (double)1000));
+        statusWriter.write(String.format("Memory_usage:%.2f\n", memu.mur.maxConsumationMemory));
+        statusWriter.write("Haui_count:"+ memu.hauiCount +"\n");
+        statusWriter.write("Join_count:"+ memu.candidateCount +"\n");
+
+//		test();
+	}
+	
+	
+	public static void test() throws Exception{
+		String location = "E:\\Data_mining\\dataset\\have_value_table\\";
+		String fileName= "T10I4N4KD200K_UM";
+		String quantityDBPath = location+fileName+".txt";
+		String profitPath=location+fileName+"_UtilityTable.txt";
+		String HAUIsPath = "E:\\java_workspace\\MEMU\\output\\with_lub.txt";
+		int GLMAU = 200000; // global least minimum average utility
+		int beta= 5000;
+
+		long startTimestamp = System.currentTimeMillis();
+
+		// Applying the MEMU algorithm
 		MEMU memu = new MEMU();
 		memu.runAlgorithm(HAUIsPath, profitPath, quantityDBPath, beta, GLMAU);
 		
